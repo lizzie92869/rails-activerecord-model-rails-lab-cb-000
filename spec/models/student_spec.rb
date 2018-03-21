6c9ec60642ae719b7efaa4ea352372cd70cd627a
Lizzie <lizzie.sorin@hotmail.fr>
2018-03-21 09:56:42 -0700

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe Student do
-  before(:each) do
-    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-  end
+it 'can be created' do
+student = Student.create!(first_name: "Lizzie", last_name: "Sorin")
+expect(student).to be_valid
+end
 
-  it 'can be created' do
-    expect(@student).to be_valid
-  end
 
-  it 'has a to_s instance method' do
-    expect(@student.to_s).to eq("Daenerys Targaryen")
-  end
+it 'has a to_s method' do
+student = Student.create!(first_name: "Lizzie", last_name: "Sorin")
+expect(student.to_s).to eq("Lizzie Sorin")
+end
+
 end
